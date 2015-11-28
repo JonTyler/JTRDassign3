177a65b4fcc7af91c96df39d393d03a2e969605e
Rdarch <ryandarch@hotmail.com>
2015-11-28 15:09:15 -0500

    Altered

--- JonTylerRyanDarch Assign2/WordScrambleClient/JTRDarchClient.cs
+++ JonTylerRyanDarch Assign2/WordScrambleClient/JTRDarchClient.cs
@@ -12,35 +12,47 @@ namespace WordScrambleClient
         static void Main(string[] args)
         {
             IWordScrambleGame game = new WordScrambleClient.WordScrambleGameService.WordScrambleGameClient();
+            WordScrambleGameClient proxy = new WordScrambleGameClient();
+            bool canPlayGame = true;
+            Console.WriteLine("Player's name?");
+            String playerName = Console.ReadLine();
 
-            Word scrambledWord = game.getScrambledWord();
-
-            Console.WriteLine("Can you unscramble this word?");
-
-            Console.WriteLine("=> " + scrambledWord.scrambledWord);
-
-            string guessedWord = Console.ReadLine();
-
-            if (game.guessWord(guessedWord, scrambledWord.unscrambledWord))
-
+            if (!proxy.isGameBeingHosted())
             {
-
-                Console.WriteLine("Wow, You Won! ;-)");
-
+                Console.WriteLine("Welcome " + playerName +
+                "! Do you want to host the game?");
+                if (Console.ReadLine().CompareTo("yes") == 0)
+                {
+                    Console.WriteLine("Type the word to scramble.");
+                    string inputWord = Console.ReadLine();
+                    string scrambledWord = proxy.hostGame(playerName, "", inputWord);
+                    canPlayGame = false;
+                    Console.WriteLine("You're hosting the game with word '" + inputWord + "' scrambled as '" + scrambledWord + "'");
+                    Console.ReadKey();
+                }
             }
 
-            else
-
+            if (canPlayGame)
             {
+                Console.WriteLine("Do you want to play the game?");
+                if (Console.ReadLine().CompareTo("yes") == 0) {
+                Word gameWords = proxy.join(playerName);
+                Console.WriteLine("Can you unscramble this word? => " + gameWords.scrambledWord);
+                String guessedWord;
 
-                Console.WriteLine("Sorry, You Lose :-( You get NOTHING. Good DAY sir!");
-
+                bool gameOver = false;
+                while (!gameOver)
+                {
+                    guessedWord = Console.ReadLine();
+                    gameOver = proxy.guessWord(playerName, guessedWord, gameWords.unscrambledWord);
+                    if (!gameOver)
+                    {
+                        Console.WriteLine("Nope, try again...");
+                    }
+                }
+                Console.WriteLine("You WON!!!");
+                }
             }
-
-            Console.ReadKey();
-
-           }
-
         }
-
+    }
 }
